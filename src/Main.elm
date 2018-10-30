192550c966e22454d615c17d7f705656480ea7e0
TheDuffman <rohman255@gmail.com>
2018-10-30 15:45:44 +1000

    stuff

--- src/Main.elm
+++ src/Main.elm
@@ -1,2 +1,40 @@
-port module Main exposing (..)
+port module Main exposing (Entry, Model, main, view)
 
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+--https://guide.elm-lang.org/architecture/text_fields.html
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { entries : List Entry
+    , input : String
+    }
+
+
+type alias Entry =
+    { description : String }
+
+
+
+-- UPDATE
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input ]
