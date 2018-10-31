30e2f4357de8eb85e20306a38d278550c900e4ea
TheDuffman <rohman255@gmail.com>
2018-10-31 12:27:41 +1000

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,14 @@
 port module Main exposing (Entry, Model, main, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, Attribute, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
+
+
 
 --https://guide.elm-lang.org/architecture/text_fields.html
 --https://github.com/evancz/elm-todomvc/blob/master/src/Main.elm
-
-
 -- MAIN
 
 
@@ -28,18 +29,29 @@ type alias Model =
 type alias Entry =
     { description : String }
 
+
 init : Model
 init =
     { entries = []
     , input = ""
     }
 
+
+
 -- UPDATE
+
+
 type Msg
-    =
+    = AddToDo
+
 
 update : Msg -> Model -> Model
 update msg model =
+    case msg of
+        AddToDo newEntry ->
+            { model | entry = newEntry }
+
+
 
 -- VIEW
 
@@ -47,4 +59,5 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ input ]
+        [ input [ placeholder "Add To Do", value model.entry, onInput AddToDo ] ] []
+        , div [] [ text model.entry ]
